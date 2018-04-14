9ff9d173bf59a58572f3922f7d27be37fbd737e5
JulianChu <yulang.chu@gmail.com>
2018-04-14 18:42:57 +0200

    add tests

--- server/server.js
+++ server/server.js
@@ -5,9 +5,24 @@ app.get("/", (req, res)=>{
   res.status(200).send("Hello World");
 });
 
+app.get("/users", (req,res)=>{
+  let users = [{ 
+    name:'Tom',
+    gender: 'male'
+  }, {
+    name: 'Jack',
+    gender: 'male'
+  },{
+    name: 'John',
+    gender: 'male'
+}]
+  res.status(200).send(users);
+})
+
 app.get("/err", (req, res)=>{
   res.status(404).send({
-    error: "Page not found."
+    error: "Page not found.",
+    name: 'Test app'
   })
 })
 
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,19 +1,52 @@
 const request = require("supertest");
 const app = require("../server/server").app;
 
-it('should return hello world', (done)=>{
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Hello World')
-    .end(done);
-});
+describe('Server', () => {
+      describe('GET /', () => {
+        it('should return hello world', (done) => {
+          request(app)
+            .get('/')
+            .expect(200)
+            .expect('Hello World')
+            .end(done);
+        });
+      }),
 
-it('should return page not found',(done)=>{
-  request(app)
-    .get("/err")
-    .expect(404)
-    .expect({error: "Page not found."})
-    .end(done);
+      describe('GET /users', () => {
+        it('should return users', (done) => {
+          let users = [{
+            name: 'Tom',
+            gender: 'male'
+          }, {
+            name: 'Jack',
+            gender: 'male'
+          }, {
+            name: 'John',
+            gender: 'male'
+          }]
+          request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+              expect(res.body).toEqual(expect.arrayContaining(users))
+            })
+            .end(done)
+        })
+      })
+    },
 
-})
+    describe('GET /err', () => {
+        it('should return page not found', (done) => {
+            request(app)
+              .get("/err")
+              .expect(404)
+              .expect((res) => {
+                expect(res.body).toMatchObject({
+                  error: 'Page not found.'
+                })
+              })
+              .end(done);
+          }
+        )}
+      )
+    )
